9a4abf87d4786f9c8ef94bcc6d3261bc55161ff0
bencogl <ben.coglitore.srg@gmail.com>
2025-05-07 16:37:15 +0200

    Update excel_utils.py

--- excel_utils.py
+++ excel_utils.py
@@ -1,24 +1,37 @@
 from openpyxl import load_workbook
-from decimal import Decimal as D
+from datetime import datetime as D
 
-def to_num(s):
-    return D(s.replace('.', '').replace(',', '.'))
+def to_num(value):
+    try:
+        return float(value)
+    except ValueError:
+        return value
 
 def extract_from_excel(filepath):
     wb = load_workbook(filepath, data_only=True)
-    data = []
+    data = {}
     for ws in wb.worksheets:
         if ws['A1'].value == 'Codice prodotto':
             for row in ws.iter_rows(min_row=2, values_only=True):
+                codice_listino = row[3] if row[3] else ""
+                
+                # Assicurati di avere un Codice Listino
+                if not codice_listino:
+                    continue
+                
                 listino_data = {
                     "filename": filepath,
-                    "Codice Listino": row[3] if row[3] else "",
+                    "Codice Listino": codice_listino,
                     "Prodotto": row[1] if row[1] else "",
-                    "Data fine vendibilità": row[10].strftime('%d/%m/%Y') if row[10] else "",
+                    "Data fine vendibilità": row[10].strftime('%d/%m/%Y') if isinstance(row[10], D) else ""
                 }
+
                 # Conversione delle colonne numeriche
                 for i, cell in enumerate(row):
                     if cell is not None and isinstance(cell, (int, float, D)):
                         listino_data[f"Colonna_{i}"] = to_num(str(cell))
-                data.append(listino_data)
+                
+                # Usa il Codice Listino come chiave
+                data[codice_listino] = listino_data
+
     return data
