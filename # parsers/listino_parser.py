a3b3cf6bb1e41f96793b139b4a0abfa942bcca19
bencogl <ben.coglitore.srg@gmail.com>
2025-05-07 11:55:42 +0200

    Create listino_parser.py

---
+++ # parsers/listino_parser.py
@@ -0,0 +1,32 @@
+from decimal import Decimal as D
+
+def to_num(s):
+    return D(s.replace('.', '').replace(',', '.'))
+
+def get_listino_parser(filename, content, knowledge):
+    for listino in knowledge['listini']:
+        if any(marker in filename for marker in listino['riconoscimento']['nome_file']):
+            return ListinoParser(listino)
+    return None
+
+class ListinoParser:
+    def __init__(self, config):
+        self.config = config
+
+    def parse(self, content):
+        data = {}
+        for campo in self.config['campi']:
+            if 'valore_fisso' in campo:
+                data[campo['nome']] = campo['valore_fisso']
+            elif 'posizione' in campo:
+                for pos in campo['posizione']:
+                    try:
+                        data[campo['nome']] = to_num(content[pos[0]][pos[1]])
+                    except (IndexError, ValueError):
+                        continue
+            elif 'formula' in campo:
+                try:
+                    data[campo['nome']] = eval(campo['formula'])
+                except Exception as e:
+                    print(f"Errore nel calcolo della formula per {campo['nome']}: {e}")
+        return data
